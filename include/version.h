41a1e78eb9db79d12fbd51fe00b0d797923bb3c0
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2023-07-25 03:41:29 +0200

    Build

--- include/version.h
+++ include/version.h
@@ -1,13 +1,13 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "1010"
+  #define BUILD_NUMBER "1011"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.1010 - 2023-07-25 03:37:14.357762"
+  #define VERSION "v1.0.1011 - 2023-07-25 03:40:17.189477"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0.1010"
+  #define VERSION_SHORT "v1.0.1011"
 #endif
 #ifndef BUILD_DATETIME
-  #define BUILD_DATETIME "2023-07-25 03:37:14.357762"
+  #define BUILD_DATETIME "2023-07-25 03:40:17.189477"
 #endif
