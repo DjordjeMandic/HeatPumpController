6717e24f6b6c530bc8eef5fcc3a7ebcc12a17d6a
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2023-08-24 03:55:20 +0200

    Fix compilation error, update gitignore

--- include/version.h
+++ include/version.h
@@ -1,13 +1,13 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "1031"
+  #define BUILD_NUMBER "1032"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.1031 - 2023-08-24 02:55:35.184703"
+  #define VERSION "v1.0.1032 - 2023-08-24 03:45:28.308390"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0.1031"
+  #define VERSION_SHORT "v1.0.1032"
 #endif
 #ifndef BUILD_DATETIME
-  #define BUILD_DATETIME "2023-08-24 02:55:35.184703"
+  #define BUILD_DATETIME "2023-08-24 03:45:28.308390"
 #endif
