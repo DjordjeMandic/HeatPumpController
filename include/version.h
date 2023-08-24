e56f3e0f491108658cfc470d1e0914b441e64279
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2023-08-24 03:38:59 +0200

    Update gitignore

--- include/version.h
+++ include/version.h
@@ -1,13 +1,13 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "1027"
+  #define BUILD_NUMBER "1031"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0.1027 - 2023-07-25 04:07:50.182114"
+  #define VERSION "v1.0.1031 - 2023-08-24 02:55:35.184703"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0.1027"
+  #define VERSION_SHORT "v1.0.1031"
 #endif
 #ifndef BUILD_DATETIME
-  #define BUILD_DATETIME "2023-07-25 04:07:50.182114"
+  #define BUILD_DATETIME "2023-08-24 02:55:35.184703"
 #endif
